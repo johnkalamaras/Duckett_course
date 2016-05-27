c4f82a2c77f477b9bd72ddaf322c11032b17ae7f
johnkalamaras <jkalamar@hotmail.com>
2016-05-27 16:37:12 +0300

    Changed function from an anonymous to just "showPosition" (Line 21) +Correct bugs

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -8,12 +8,12 @@ function charCount(e){
     var textEntered, charDisplay, counter, lastkey;
     textEntered = document.getElementById('message').value;
     charDisplay = document.getElementById('charactersLeft');
-    counter = (180-textEntered.length);
+    counter = (180-(textEntered.length));
     charDisplay.textContent = counter;
     
     lastkey = document.getElementById('lastkey');
     lastkey.textContent = 'Last key in ASCII code: ' + e.keyCode;
 }
 var el;
-el = document.getElementById(message);
-el.addEventListener('keypress',charCount,false);
+el = document.getElementById('message');
+el.addEventListener('keypress',function(){charCount(e)},false);
